eae7c4c70ed8d396c8b61c3148dfe1fd1e53cd96
Simeon Simeonov <simeonsimeonov006@gmail.com>
2022-07-03 09:10:25 +0300

    Using a single js and a single css files

--- js/util/lib.js
+++ js/util/lib.js
@@ -1,6 +1,7 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import page from '../../node_modules/page/page.mjs';
 import Swal from '../../node_modules/sweetalert2/dist/sweetalert2.all.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 
 export {
